fbee62b2981a4110fbe98521d452087232c9b9fd
AdarshGrewal <adarshgrewal@gmail.com>
2023-10-29 11:35:42 +0530

    lyriq: Setup kernel parameters
    Change-Id: I08e7a38d1683faac3c93186e3c9e0f4d96fe6d9e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,5 +25,37 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 TARGET_BOOTLOADER_BOARD_NAME := lyriq
 TARGET_NO_BOOTLOADER := true
 
+# Kernel
+BOARD_BOOT_HEADER_VERSION := 3
+BOARD_KERNEL_IMAGE_NAME := Image.gz
+BOARD_KERNEL_PAGESIZE := 4096
+
+BOARD_DTB_OFFSET := 0x07c08000
+BOARD_KERNEL_BASE := 0x40078000
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_KERNEL_TAGS_OFFSET := 0x07c08000
+BOARD_RAMDISK_OFFSET := 0x11088000
+
+BOARD_MKBOOTIMG_ARGS :=	\
+    --header_version $(BOARD_BOOT_HEADER_VERSION) \
+    --kernel_offset $(BOARD_KERNEL_OFFSET) \
+    --ramdisk_offset $(BOARD_RAMDISK_OFFSET) \
+    --tags_offset $(BOARD_KERNEL_TAGS_OFFSET) \
+    --dtb_offset $(BOARD_DTB_OFFSET)
+
+TARGET_KERNEL_SOURCE := kernel/motorola/lyriq
+
+TARGET_KERNEL_CONFIG := \
+    k6891v1_64_k419_d1180_defconfig \
+    ext_config/moto-k6891v1_64_k419_d1180.config \
+    ext_config/moto-k6891v1_64_k419_d1180-lyriq.config
+
+BOARD_KERNEL_CMDLINE := \
+    bootopt=64S3,32N2,64N2 \
+    androidboot.bootdevice=11270000.ufshci
+
 # Platform
 TARGET_BOARD_PLATFORM := mt6893
+
+# Recovery
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
