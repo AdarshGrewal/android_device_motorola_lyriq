6b0d9a6ae7d75b0f555064711ca63bb4e46cb369
AdarshGrewal <adarshgrewal@gmail.com>
2023-11-04 18:30:25 +0530

    lyriq: Inherit 6GB dalvik config
    Change-Id: I211851ce21130577569a169989713513fe705107

--- device.mk
+++ device.mk
@@ -13,6 +13,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Enable Virtual A/B OTA with vendor ramdisk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # A/B
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl \
