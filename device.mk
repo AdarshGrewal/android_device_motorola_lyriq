973c65eda4d621e4fcd2bba385dd9719684e4568
AdarshGrewal <adarshgrewal@gmail.com>
2023-11-05 09:39:23 +0530

    lyriq: Get rid of sku setup
     * this only seems to be used for nfc and ds/ss handling
    
    Change-Id: Icb3118c3a92c776aea785c53934d73b1f23601ca

--- device.mk
+++ device.mk
@@ -197,7 +197,6 @@ PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.2.vendor
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
     frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml
 
 # Sensors
