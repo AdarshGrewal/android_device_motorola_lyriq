888ed5692d0860ce99900464af5a60d7139600b1
AdarshGrewal <adarshgrewal@gmail.com>
2023-11-08 12:08:16 +0530

    aa
    Change-Id: Ia36fc1b592864a698a6e5c8f5a5ac3264f6bea29

--- fingerprint/BiometricsFingerprint.h
+++ fingerprint/BiometricsFingerprint.h
@@ -25,6 +25,7 @@
 #include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
 #include <android/hardware/biometrics/fingerprint/2.3/IBiometricsFingerprint.h>
+#include <vendor/goodix/hardware/biometrics/fingerprint/2.1/IGoodixFingerprintDaemon.h>
 
 namespace android {
 namespace hardware {
@@ -43,6 +44,7 @@ using ::android::hardware::Void;
 using ::android::hardware::hidl_vec;
 using ::android::hardware::hidl_string;
 using ::android::sp;
+using ::vendor::goodix::hardware::biometrics::fingerprint::V2_1::IGoodixFingerprintDaemon;
 
 struct BiometricsFingerprint : public IBiometricsFingerprint {
 public:
@@ -78,6 +80,7 @@ private:
     std::mutex mClientCallbackMutex;
     sp<IBiometricsFingerprintClientCallback> mClientCallback;
     fingerprint_device_t *mDevice;
+    sp<IGoodixFingerprintDaemon> mGoodixFingerprintDaemon;
 };
 
 }  // namespace implementation
