dbcd96df4381b95e9740792d0f6c7b67bd92ffae
AdarshGrewal <adarshgrewal@gmail.com>
2023-11-12 17:39:46 +0530

    lyriq: Cleanup media blobs
    Change-Id: I835866d87b236e0102803eba8a6927f74d040adf

--- device.mk
+++ device.mk
@@ -228,6 +228,12 @@ PRODUCT_PACKAGES += \
     android.hardware.light-V1-ndk_platform.vendor
 
 # Media
+PRODUCT_PACKAGES += \
+    libavservices_minijail_vendor \
+    libcodec2_hidl@1.2.vendor \
+    libcodec2_soft_common.vendor \
+    libsfplugin_ccodec_utils.vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
